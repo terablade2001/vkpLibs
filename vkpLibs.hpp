94b089af3945e3d41d945c40b59e312f3a65a5e8
terablade2001 <terablade2001@yahoo.gr>
2021-04-30 23:00:19 +0300

    (0.207): Incorporating a master vkpLibs.hpp file.

---
+++ vkpLibs.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "vkpBuildVersioner/C++/vkpBuildVersioner.hpp"
+#include "vkpCircularBuffer/vkpCircularBuffer.hpp"
+#include "vkpConfigReader/src/vkpConfigReader.hpp"
+#include "vkpProgressBar/vkpProgressBar.hpp"
+#include "vkpTimer/vkpTimer.hpp"
+#include "vkpOpmlCPP/vkpOpmlCPP.hpp"
--- vkpVersion.hpp
+++ vkpVersion.hpp
@@ -1,2 +1,2 @@
 #pragma once
-#define __VKPLIBS__VERSION "[0.206]"
+#define __VKPLIBS__VERSION "[0.207]"
